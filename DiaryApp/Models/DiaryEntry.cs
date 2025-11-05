fca58850d0651362d8240198c5c641e3264a4678
Alvin <alvin.webdev.consular@gmail.com>
2025-11-06 03:00:54 +0800

    Update to .NET 8.0 for deployment compatibility

--- DiaryApp/Models/DiaryEntry.cs
+++ DiaryApp/Models/DiaryEntry.cs
@@ -4,22 +4,21 @@ namespace DiaryApp.Models
 {
     public class DiaryEntry
     {
-        //[Key]
+        [Key]
         public int Id { get; set; }
-        [Required(ErrorMessage = "Please enter a title!")]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 100 characters!")]
+        
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Please enter a content!")]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Content must be between 3 and 100 characters!")]
+        [Required]
+        [StringLength(1000)]  // Explicit length for PostgreSQL
         public string Content { get; set; } = string.Empty;
 
         [Required]
-        [Display(Name = "Date")]
-        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}", ApplyFormatInEditMode = false)]
         public DateTime Created { get; set; } = DateTime.Now;
 
         [Required]
-        public Boolean IsActive { get; set; } = true;
+        public bool IsActive { get; set; } = true;
     }
 }
